9bfb575c193a86f5246f891105f821e279d8d038
k-ketsu <motazwageeh12345@gmail.com>
2026-06-17 14:28:41 -0700

    .

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const TheURL = "http://localhost:3000"
+const TheURL = "test2-production-ae19.up.railway.app"
 const Age = document.getElementById("AddAge").addEventListener("input", function () {
     this.value = this.value.replace(/[^0-9]/g, "");
 })
