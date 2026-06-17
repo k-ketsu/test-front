ba9577a7af6779e8bdd6edccb64f8d7b2d9de590
k-ketsu <motazwageeh12345@gmail.com>
2026-06-17 11:19:57 -0700

    the front

---
+++ frontend/script.js
@@ -0,0 +1,36 @@
+
+const TheURL = "http://localhost:3000"
+const Age = document.getElementById("AddAge").addEventListener("input", function () {
+    this.value = this.value.replace(/[^0-9]/g, "");
+})
+document.getElementById("TestForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const name = document.getElementById("AddName")
+    const Age = document.getElementById("AddAge")
+    if (name.value == "" || Age.value == null) {
+        console.log("nothing To input")
+    }
+    else {
+        const ReqData = {
+            NewName: name.value,
+            NewAge: Age.value
+        }
+        const req = await fetch(TheURL + "/AddNew", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(ReqData)
+        })
+        const res = await req.json()
+        if(res.status == "ok"){
+            console.log("done")
+        }
+        else{
+            console.log("something went wrong")
+        }
+        
+    }
+
+
+})
